f272654bb33222cd07316f04ed2edaccd8617457
Иван Недзвецкий <stels-cs@yandex.ru>
2020-12-10 21:52:51 +0300

    3.0.14
    Повтор запросов к апи в случае 10 ошибки

--- src/index.js
+++ src/index.js
@@ -233,6 +233,7 @@ export const VK_API_AUTH_FAIL = 5
 export const VK_API_UNKNOWN_ERROR = 1
 export const VK_API_TOO_MANY_REQUEST = 6
 export const VK_API_TOO_MANY_SAME_ACTIONS = 9
+export const VK_API_INTERNAL_ERROR = 10
 
 /*
 Считается @in что если вызов апи вернулся с этим кодом, то запрос можно повторить
@@ -241,6 +242,7 @@ export const SOFT_ERROR_CODES = [
 	VK_API_UNKNOWN_ERROR, //Произошла неизвестная ошибка.
 	VK_API_TOO_MANY_REQUEST, //Слишком много запросов в секунду.
 	VK_API_TOO_MANY_SAME_ACTIONS, //Слишком много однотипных действий.
+	VK_API_INTERNAL_ERROR, //Неизвестная ошибка, но можно повторить еще раз
 ]
 
 export function delay(time) {
